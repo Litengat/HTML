51e9ae12ddd2b9b4ec5e6bdbdc5764e0766dae77
Litengat <107832821+Litengat@users.noreply.github.com>
2023-09-14 17:47:13 +0000

    main

--- 07Tabellen/main.js
+++ 07Tabellen/main.js
@@ -1,25 +1,27 @@
 document.body.onload = addElement();
 
 function addElement() {
-const stunden = ["Latein","Mathe","Englisch","Chemie","Erdkunde","Musik"]
-const newtable = document.createElement("tabele");
+    const stunden = ["Latein","Mathe","Englisch","Chemie","Erdkunde","Musik"]
+    const newtable = document.createElement("tabele");
 
-for (let i = 0; i < 5; i++) {
-    var newtr = document.createElement("tr")
-    for(let i = 0;i < 6;i++){
-        var newtd = document.createElement("td");
-        var newContent = document.createTextNode(stunden[getRandomInt(stunden.length)]);
-        console.log(newContent);
-        newtd.appendChild(newContent);
-        newtr.appendChild(newtd)
+    for (let i = 0; i < 5; i++) {
+        var newtr = document.createElement("tr")
+        for(let i = 0;i < 6;i++){
+            var newtd = document.createElement("td");
+            var newContent = document.createTextNode(stunden[getRandomInt(stunden.length)]);
+            console.log(newContent);
+            newtd.appendChild(newContent);
+            newtr.appendChild(newtd)
+        }
+        newtable.appendChild(newtr);
     }
-    newtable.appendChild(newtr);
+    newtable.setAttribute("Border", "1")
+    const currentDiv = document.getElementById("plan");
+    console.log(currentDiv)
+    document.body.insertBefore(newtable, currentDiv);
+    
 }
-const currentDiv = document.getElementById("plan");
-console.log(currentDiv)
-document.body.insertBefore(newtable, currentDiv);
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
-  }
 }
